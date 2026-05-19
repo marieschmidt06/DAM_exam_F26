e1394cf4857a133a64905f2683cd0dbef58a66b6
marieschmidt06 <marieschmidt1106@gmail.com>
2026-05-19 16:50:45 +0200

    Add files via upload

--- Final_project_demographic_development/r_scripts/hypothesis_A_C_D_E_F.R
+++ Final_project_demographic_development/r_scripts/hypothesis_A_C_D_E_F.R
@@ -14,7 +14,7 @@ total_1834 <- read_csv2("data/1834_total.csv", na="NULL")
 total_1860 <- read_csv2("data/1860_total.csv", na="NULL")
 
 #create objects with selected rows
-#add column for shire in 1801 and 1834
+  #add column for shire in 1801 and 1834
 clean_1801 <- total_1801 %>%
   filter(amt=="Århus") %>% 
   select(amt, sogn, koen, alder, civilstand) %>% 
@@ -49,23 +49,26 @@ clean_1860 <- total_1860 %>%
   filter(amt=="Århus") %>% 
   select(amt, herred, sogn, koen, alder, civilstand)
 
-#combining datasets
-#add column for year
+#combine datasets
+  #add column for year
 all_census <- bind_rows(
   mutate(clean_1801, år = 1801),
   mutate(clean_1834, år = 1834),
   mutate(clean_1860, år = 1860)
 )
 
+#download the new combined dataset as CSV
+write.csv(all_census,"~/Downloads/aarhus_census_all.csv", row.names = FALSE)
+
 
   ## Hypothesis A ##
 
-#create object, count inhabitants in each shire each year
-inhab_shire <- all_census %>% 
+#count inhabitants in Aarhus County
+inhab_shires <- all_census %>% 
   count(herred,år)
 
-#plot
-ggplot(inhab_shire,
+#plot inhabitants of Aarhus County  
+ggplot(inhab_shires,
        aes(x=år,y=n,color=herred))+
   geom_line(size = 0.5) +
   geom_point(size = 1) +
@@ -74,20 +77,20 @@ ggplot(inhab_shire,
     axis.text.x = element_text(size = 8),
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 13))+
-  labs(title = "Population growth: Aarhus county",
+  labs(title = "Population growth: Aarhus County",
        x = "Year",
        y = "Population",
-       color = "Shire")
+       color = "Shire:")
 
-ggsave("pop_grow_county.png")
+ggsave("pop_grow_aarhus.png")
 
-#Same but only Hasle shire 
-inhab_hasle <- all_census %>% 
+#count inhabitants in Hasle Shire  
+inhab_has_parish <- all_census %>% 
   filter(herred=="Hasle") %>% 
   count(sogn,år)
 
-#plot
-ggplot(inhab_hasle,
+#plot inhabitants of Hasle Shire  
+ggplot(inhab_has_parish,
        aes(x=år,y=n,color=sogn))+
   geom_line(size = 0.5) +
   geom_point(size = 1) +
@@ -96,26 +99,26 @@ ggplot(inhab_hasle,
     axis.text.x = element_text(size = 8),
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 13))+
-  labs(title = "Population growth: Hasle shire",
+  labs(title = "Population growth: Hasle Shire",
        x = "Year",
        y = "Population",
-       color = "Parish")
+       color = "Parish:")
 
 ggsave("pop_grow_hasle.png")
 
 
   ## Hypothesis C ##
 
-#create object, calculate average age in each shire
-inhab_age <- all_census %>%
+#calculate average age in Aarhus County
+inhab_age_aar <- all_census %>%
   filter(år %in% c(1801, 1834, 1860)) %>%
   group_by(år,herred) %>%
   summarise(
     average_age = mean(alder, na.rm = TRUE)
   )
 
-#plot it
-ggplot(inhab_age,
+#plot average age in Aarhus County
+ggplot(inhab_age_aar,
        aes(x = år,
            y = average_age,
            group = herred,
@@ -128,16 +131,16 @@ ggplot(inhab_age,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 13))+
   labs(
-    title = "Average age over time in Aarhus county",
+    title = "Average age in Aarhus County",
     x = "Year",
     y = "Average age",
-    color = "District"
+    color = "Shire:"
   )
 
 ggsave("age_aarhus.png")
 
-#Calculate average age in Hasle shire 
-inhab_age_hasle <- all_census %>%
+#calculate average age in Hasle Shire 
+inhab_age_has <- all_census %>%
   filter(herred=="Hasle") %>% 
   filter(år %in% c(1801, 1834, 1860)) %>%
   group_by(år,sogn) %>%
@@ -145,8 +148,8 @@ inhab_age_hasle <- all_census %>%
     average_age = mean(alder, na.rm = TRUE)
   )
 
-#plot it
-ggplot(inhab_age_hasle,
+#plot average age in Hasle Shire
+ggplot(inhab_age_has,
        aes(x = år,
            y = average_age,
            group = sogn,
@@ -159,10 +162,10 @@ ggplot(inhab_age_hasle,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 13))+
   labs(
-    title = "Average age over time in Hasle shire",
+    title = "Average age in Hasle Shire",
     x = "Year",
     y = "Average age",
-    color = "Parish"
+    color = "Parish:"
   )
 
 ggsave("age_hasle.png")
@@ -170,49 +173,35 @@ ggsave("age_hasle.png")
 
   ## Hypothesis D ##
 
-#check witch labels are included in the dataset
+#check which labels are included in the dataset
 unique(all_census$civilstand)
 unique(all_census$koen)
 
-#categorize labels as married 
-all_census <- all_census %>%
-  mutate(
-    civilstand = case_when(
-      civilstand %in% c("enke", "separeret", "enkemand", "skilt", "forlovet") ~ "gift",
-      TRUE ~ civilstand
-    )
-  )
-
-#create object, count number of married and unmarried in Aarhus county
-  #calculate average age for both groups 
-mean_marriage <- all_census %>%
+#calculate average age of unmarried inhabitants in Aarhus County 
+avrg_unm_aar <- all_census %>%
   filter(
     alder >= 15,
     alder <= 50,
-    civilstand %in% c("gift", "ugift"), 
+    civilstand=="ugift", 
     civilstand != "",
     koen != "") %>%
-  group_by(år, herred, civilstand) %>%
+  group_by(år, herred) %>%
   summarise(antal=n(),
     gennemsnitsalder = mean(alder, na.rm = TRUE))
 
-#plot it
-ggplot(mean_marriage,
+#plot average age of unmarried inhabitants in Aarhus County
+ggplot(avrg_unm_aar,
        aes(x = år,
            y = gennemsnitsalder,
-           color = civilstand,
-           group=civilstand)) +
+           color = herred,
+           group=herred)) +
   geom_line(size = 0.5) +
   geom_point(size = 1) +
-  facet_wrap(~herred) +
   labs(
-    title = "Average age of marriage: Aarhus county",
+    title = "Average age as unmarried: Aarhus County",
     x = "Year",
     y = "Average age",
-    color = "Marital status")+
-  scale_color_discrete(labels = c(
-        "Married",
-        "Unmarried"))+
+    color = "Shire:")+
   theme_bw()+
   theme(
     axis.text.x = element_text(size = 8),
@@ -220,38 +209,34 @@ ggplot(mean_marriage,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 15))
 
-ggsave("mean_marry_aarhus.png",width = 8, height = 6)
+ggsave("avrg_age_unm_aar.png",width = 8, height = 6)
 
-#Do the same for Hasle shire only 
-mean_marriage_hasle <- all_census %>%
+#Calculate average age of unmarried inhabitants in Hasle Shire
+avrg_unm_has <- all_census %>%
   filter(
     alder >= 15,
     alder <= 50,
-    civilstand %in% c("gift", "ugift"),
+    civilstand=="ugift",
     herred=="Hasle",
     civilstand != "",
     koen != "") %>%
-  group_by(år, sogn, civilstand) %>%
+  group_by(år, sogn) %>%
   summarise(antal=n(),
             gennemsnitsalder = mean(alder, na.rm = TRUE))
 
-#plot it
-ggplot(mean_marriage_hasle,
+#plot average age of unmarried inhabitants in Hasle Shire
+ggplot(avrg_unm_has,
        aes(x = år,
            y = gennemsnitsalder,
-           color = civilstand,
-           group=civilstand)) +
+           color = sogn,
+           group=sogn)) +
   geom_line(size = 0.5) +
   geom_point(size = 1) +
-  facet_wrap(~sogn, ncol=4) +
   labs(
-    title = "Average age of marriage: Hasle shire",
+    title = "Average age as unmarried: Hasle Shire",
     x = "Year",
     y = "Average age",
-    color = "Marital status")+
-  scale_color_discrete(labels = c(
-    "Married",
-    "Unmarried"))+
+    color = "Parish:")+
   theme_bw()+
   theme(
     axis.text.x = element_text(size = 8),
@@ -259,13 +244,13 @@ ggplot(mean_marriage_hasle,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 15))
 
-ggsave("mean_marriage_hasle.png",width = 8, height = 6)
+ggsave("avrg_age_unm_has.png",width = 8, height = 6)
 
 
   ## Hypothesis E ##
 
-#object, calculate share of elders in Aarhus county
-plot_old_aarhus <- all_census %>%
+#Calculate share of elders in Aarhus County shires 
+plot_o_aar <- all_census %>%
   filter(alder != "") %>%
   group_by(år, herred) %>%
   summarise(
@@ -274,18 +259,44 @@ plot_old_aarhus <- all_census %>%
     procent_over_60 = over_60 / total_befolkning * 100
   )
 
-#plot it
-ggplot(plot_old_aarhus,
+#calculate the total share of elders in Aarhus County
+o_total_aar <- all_census%>%
+  filter(alder != "",) %>%
+  group_by(år) %>%
+  summarise(
+    total_befolkning = n(),
+    over_60 = sum(alder > 60),
+    procent_over_60 = over_60 / total_befolkning * 100
+  )
+
+#plot share of elders in shires with total line 
+ggplot(plot_o_aar,
        aes(x = år,
            y = procent_over_60,
            group=herred,
            color = herred)) +
   geom_line(size = 0.5) +
   geom_point(size = 1)+
+  geom_line(
+    data = o_total_aar,
+    aes(x = år,
+        y = procent_over_60),
+    inherit.aes=FALSE,
+    color = "black",
+    linetype = "dashed",
+    size = 1) +
+  geom_point(
+    data = o_total_aar,
+    aes(x = år,
+        y = procent_over_60),
+    inherit.aes=FALSE,
+    color = "black",
+    size = 2
+  ) +
   labs(
-    title = "Elder population: Aarhus county",
+    title = "Population (>60 years): Aarhus County",
     x = "Year",
-    y = "Percentage share of elders (>60)",
+    y = "Percentage share",
     color = "Shire:")+
   theme_bw()+
   theme(
@@ -294,10 +305,10 @@ ggplot(plot_old_aarhus,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 17)) 
 
-ggsave("above_60_aarhus.png", width = 8, height = 8)
+ggsave("old_aarhus.png", width = 8, height = 8)
 
-#Same for Hasle shire only
-plot_old_hasle <- all_census %>%
+#calculate share of elders in Hasle Shire parishes  
+plot_o_has <- all_census %>%
   filter(herred=="Hasle",
          alder != "") %>%
   group_by(år, sogn) %>%
@@ -307,19 +318,46 @@ plot_old_hasle <- all_census %>%
     procent_over_60 = over_60 / total_befolkning * 100
   )
 
-#plot it
-ggplot(plot_old_hasle,
+#calculate total share of elders in Hasle Shire parishes
+o_total_has <- all_census%>%
+  filter(alder != "",
+         herred=="Hasle") %>%
+  group_by(år) %>%
+  summarise(
+    total_befolkning = n(),
+    over_60 = sum(alder > 60),
+    procent_over_60 = over_60 / total_befolkning * 100
+  )
+
+#plot share of elders in parishes with total line 
+ggplot(plot_o_has,
        aes(x = år,
            y = procent_over_60,
            group=sogn,
            color = sogn)) +
   geom_line(size = 0.5) +
   geom_point(size = 1)+
+  geom_line(
+    data = o_total_has,
+    aes(x = år,
+        y = procent_over_60),
+    inherit.aes=FALSE,
+    color = "black",
+    linetype = "dashed",
+    size = 1) +
+  geom_point(
+    data = o_total_has,
+    aes(x = år,
+        y = procent_over_60),
+    inherit.aes=FALSE,
+    color = "black",
+    size = 2
+  ) +
   labs(
-    title = "Elder population: Hasle shire",
+    title = "Population (>60 years): Hasle Shire",
     x = "Year",
-    y = "Percentage share of elders (>60)",
-    color = "Parish: ")+
+    y = "Percentage share",
+    color = "Parish:")+
   theme_bw()+
   theme(
     axis.text.x = element_text(size = 8),
@@ -327,45 +365,72 @@ ggplot(plot_old_hasle,
     panel.spacing = unit(1, "lines"),
     text = element_text(size = 17))
 
-ggsave("above_60_hasle.png", width = 8, height = 8)
+ggsave("old_hasle.png", width = 8, height = 8)
 
 
   ## Hypothesis F ##
 
-#Calculate share of children below 15 in Aarhus county
-plot_young_aarhus <- all_census %>%
-  filter(alder != "") %>%
-  group_by(år, herred) %>%
+#calculate share of children below 15 years in Aarhus County shires
+plot_y_aar <- all_census %>%
+  filter(alder != "",
+         ) %>%
+  group_by(år,herred) %>%
   summarise(
     total_befolkning = n(),
     under_15 = sum(alder < 15),
     procent_under_15 = under_15 / total_befolkning * 100
   )
 
-#plot it
-ggplot(plot_young_aarhus,
+#calculate the total share of children in Aarhus County
+y_total_aar <- all_census%>%
+  filter(alder != "",) %>%
+  group_by(år) %>%
+  summarise(
+    total_befolkning = n(),
+    under_15 = sum(alder < 15),
+    procent_under_15 = under_15 / total_befolkning * 100
+  )
+
+#plot share of children in Aarhus County shires with total line 
+ggplot(plot_y_aar,
        aes(x = år,
            y = procent_under_15,
-           group=herred,
+           group = herred,
            color = herred)) +
   geom_line(size = 0.5) +
   geom_point(size = 1)+
+  geom_line(
+    data = y_total_aar,
+    aes(x = år,
+        y = procent_under_15),
+    inherit.aes=FALSE,
+    color = "black",
+    linetype = "dashed",
+    size = 1) +
+  geom_point(
+    data = y_total_aar,
+    aes(x = år,
+        y = procent_under_15),
+    inherit.aes=FALSE,
+    color = "black",
+    size = 2
+  ) +
   labs(
-    title = "Young population: Aarhus county",
+    title = "Population (<15 years): Aarhus County",
     x = "Year",
-    y = "Percentage share of children (<15)",
-    color = "Shire")+
+    y = "Percentage share",
+    color = "Shire:")+
   theme_bw()+
   theme(
     axis.text.x = element_text(size = 8),
     legend.position = "bottom",
     panel.spacing = unit(1, "lines"),
-    text = element_text(size = 18))
+    text = element_text(size = 16))
 
-ggsave("children_aarhus.png",width = 8, height = 6)
+ggsave("young_aarhus.png",width = 8, height = 7)
 
-#Do the same for Hasle shire only
-plot_young_hasle <- all_census %>%
+#calculate share of children in Hasle Shire parishes
+plot_y_has <- all_census %>%
   filter(,
     herred=="Hasle",
     alder != "") %>%
@@ -376,24 +441,62 @@ plot_young_hasle <- all_census %>%
     procent_under_15 = under_15 / total_befolkning * 100
   )
 
-#plot it
-ggplot(plot_young_hasle,
+#calculate the total share of children in Hasle Shire
+y_total_has <- all_census%>%
+  filter(alder != "",
+         herred=="Hasle") %>%
+  group_by(år) %>%
+  summarise(
+    total_befolkning = n(),
+    under_15 = sum(alder < 15),
+    procent_under_15 = under_15 / total_befolkning * 100
+  )
+
+#plot share of children in parishes of Hasle Shire with total line 
+ggplot(plot_y_has,
        aes(x = år,
            y = procent_under_15,
            group=sogn,
            color = sogn)) +
   geom_line(size = 0.5) +
   geom_point(size = 1)+
+  geom_line(
+    data = y_total_has,
+    aes(x = år,
+        y = procent_under_15),
+    inherit.aes=FALSE,
+    color = "black",
+    linetype = "dashed",
+    size = 1) +
+  geom_point(
+    data = y_total_has,
+    aes(x = år,
+        y = procent_under_15),
+    inherit.aes=FALSE,
+    color = "black",
+    size = 2
+  ) +
   labs(
-    title = "Young population: Hasle shire",
+    title = "Population (<15 years): Hasle Shire",
     x = "Year",
-    y = "Percentage share of children (<15)",
-    color = "Parish")+
+    y = "Percentage share",
+    color = "Parish:")+
   theme_bw()+
   theme(
     axis.text.x = element_text(size = 8),
+    legend.position = "bottom",
     panel.spacing = unit(1, "lines"),
-    text = element_text(size = 18))
+    text = element_text(size = 16))
 
-ggsave("children_hasle.png", width = 8, height = 6)
+ggsave("young_hasle.png", width = 8, height = 7)
+
+
+
+
+
+
+
+
+
+
   
